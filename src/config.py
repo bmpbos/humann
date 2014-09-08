5b8a7ad9b463a73b92a45202bd208979c45c04db
Lauren McIver <Lauren McIver@localhost>
2014-09-08 12:44:26 -0600

    Merge find reactions and find pathways to reduce file reads

---
+++ src/config.py
@@ -0,0 +1,108 @@
+"""
+Configuration settings
+"""
+
+import os
+
+# software run modes
+debug = False
+verbose = False
+bypass_prescreen = False
+bypass_nucleotide_index = False
+
+# translated alignment options
+translated_alignment_choices = ["usearch","rapsearch"]
+translated_alignment_selected = translated_alignment_choices[1]
+
+# file naming
+temp_dir=""
+file_basename=""
+fasta_extension=".fa"
+
+bugs_list_name="_metaphlan_bugs_list.tsv"
+metaphlan_bowtie2_name="_metaphlan_bowtie2.txt"
+
+chocophlan_custom_database_name="_custom_chocophlan_database.ffn"
+bowtie2_index_name="_bowtie2_index"
+chocophlan_alignment_name="_bowtie2_aligned.sam"
+
+nucleotide_unaligned_reads_name_no_ext="_bowtie2_unaligned"
+nucleotide_aligned_reads_name_tsv="_bowtie2_aligned.tsv"
+
+translated_alignment_name="_aligned.tsv"
+translated_unaligned_reads_name_no_ext="_unaligned"
+
+pathabundance_file="_pathabundance.tsv"
+pathcoverage_file="_pathcoverage.tsv"
+genefamilies_file="_genefamilies.tsv"
+
+# metaphlan options
+prescreen_threshold=0.01
+metaphlan_opts=["-t","rel_ab"]
+metaphlan_pkl_file="db_v20/mpa_v20_m200.pkl"
+metaphlan_mpa_index="db_v20/mpa_v20_m200"
+
+# chocophlan formatting
+chocophlan_delimiter="|"
+chocophlan_bug_index=-2
+chocophlan_uniref_index=-1
+
+# bowtie2 options and threshold
+bowtie2_large_index_threshold=4000000000
+bowtie2_index_ext_list=[".1.bt2",".2.bt2",".3.bt2",".4.bt2",
+    ".rev.1.bt2",".rev.2.bt2"]
+bowtie2_large_index_ext=".1.bt2l"
+
+bowtie2_build_opts=[]
+bowtie2_align_opts=["--very-sensitive"]
+
+#set the locations of data in the sam file
+sam_read_name_index=0
+sam_flag_index=1
+sam_reference_index=2
+sam_mapq_index=4
+sam_read_index=9
+sam_read_quality=10
+sam_unmapped_flag=0x4
+sam_delimiter="\t"
+
+#set the locations of data in a tabulated blast formatted file
+# all translated alignment files will be of the tabulated blast format
+blast_delimiter="\t"
+blast_query_index=0
+blast_reference_index=1
+blast_identity_index=2
+blast_aligned_length_index=3
+blast_evalue_index=10
+
+# output file formats
+output_file_column_delimiter="\t"
+output_file_category_delimiter="|"
+
+# usearch options
+usearch_database_extension=".udb"
+id_threshold_default=0.4
+usearch_max_seqs=10000
+usearch_version="usearch v7.0.1001"
+usearch_opts=[]
+
+# rapsearch options
+rapsearch_database_extension=".info"
+rapsearch_opts=["-v",-1]
+
+# data files
+data_folder="data/"
+metacyc_gene_to_reactions="mcc_with_Uniref_Identifiers"
+metacyc_reactions_to_pathways="mcpc"
+reactions_database_delimiter=" "
+
+# MinPath
+minpath_file="minpath1.2.tar.gz"
+minpath_url=("http://omics.informatics.indiana.edu/mg/get.php?" + 
+    "justdoit=yes&software=" + minpath_file)
+minpath_folder="MinPath"
+minpath_script="MinPath1.2.py"
+minpath_reaction_index=0
+minpath_pathway_index=7
+minpath_pathway_delimiter=" "
+minpath_update_script="MinPath12hmp.py"
---
+++ src/quantify_modules.py
@@ -0,0 +1,219 @@
+""" 
+Generate pathway coverage and abundance
+"""
+import os, shutil, tempfile, math, re, sys
+import utilities, config, store, MinPath12hmp
+
+
+def install_minpath():
+    """
+    Download and install the minpath software
+    """
+    
+    # Download the minpath software v1.2
+    # Check to see if already downloaded
+    
+    fullpath_scripts=os.path.dirname(os.path.realpath(__file__))
+    minpath_exe=os.path.join(fullpath_scripts,config.minpath_folder,
+        config.minpath_script)
+
+    if not os.path.isfile(minpath_exe):
+        utilities.download_tar_and_extract(config.minpath_url, 
+            os.path.join(fullpath_scripts, config.minpath_file),fullpath_scripts)
+        
+    return minpath_exe
+
+
+def run_minpath(reactions_file,metacyc_datafile):
+    """
+    Run minpath on the reactions file using the datafile of pathways
+    """
+    
+    # Create temp files for the results
+    file_out, tmpfile=tempfile.mkstemp()
+    os.close(file_out)
+    
+    # Bypass minpath run if reactions file is empty
+    if os.path.getsize(reactions_file):
+    
+        file_out2, tmpfile2=tempfile.mkstemp()
+        os.close(file_out2)
+    
+        if config.verbose:
+            print "\nRun MinPath .... "
+    
+        # Redirect stdout
+        sys.stdout=open(os.devnull,"w")
+    
+        # Call minpath to identify pathways
+        MinPath12hmp.Orth2Path(infile = reactions_file, reportfile = "/dev/null", 
+            detailfile = tmpfile, whichdb = "ANY", mapfile=metacyc_datafile,
+            mpsfile = tmpfile2)
+    
+        # Undo stdout redirect
+        sys.stdout=sys.__stdout__
+    
+        utilities.remove_file(tmpfile2)
+    
+    return tmpfile
+
+
+def pathways_by_bug(args):
+    """
+    Identify the reactions from the hits found for a specific bug
+    """
+    reactions_database, hits, reactions_file, bug = args
+    
+    if config.verbose:
+        print "Identify reactions for: " + bug
+    
+    file_handle=open(reactions_file,"w")
+    
+    # Create alignments variable to identify indexes
+    alignments=store.alignments()
+    
+    query_index=alignments.find_index("query")
+    reference_index=alignments.find_index("reference")
+    evalue_index=alignments.find_index("evalue")
+    
+    # Group hits by query
+    hits_by_query={}
+    
+    for index, hit in enumerate(hits):
+        if hit[query_index] in hits_by_query:
+            hits_by_query[hit[query_index]]+=[index]
+        else:
+            hits_by_query[hit[query_index]]=[index]
+    
+    if config.verbose:
+        print "Total reads mapped to bug: " + str(len(hits_by_query))
+    
+    # Loop through hits by query
+    # Identify all hits that have a reference which is part of the database
+    total_genes={}
+    for query in hits_by_query:
+        genes=[]
+        total_score=0
+        for index in hits_by_query[query]:
+            hit=hits[index]
+            if reactions_database.gene_present(hit[reference_index]):
+                score=math.exp(-hit[evalue_index])
+                genes.append([hit[reference_index],score])
+                total_score+=score
+        # add these scores to the total gene scores
+        for gene, score in genes:
+            total_genes[gene]=score/total_score+total_genes.get(gene,0)
+                
+    # Merge the gene scores to reaction scores
+    reactions_store={}
+    for reaction in reactions_database.list_reactions():
+        genes_list=reactions_database.find_genes(reaction)
+        abundance=0
+        # Add the scores for each gene to the total score for the reaction
+        for gene in genes_list:
+            abundance+=total_genes.get(gene,0)  
+        
+        # Only write out reactions where the abundance is greater than 0
+        if abundance>0: 
+            file_handle.write(reaction+config.output_file_column_delimiter
+                +str(abundance)+"\n")
+            # Store the abundance data to compile with the minpath pathways
+            reactions_store[reaction]=abundance
+        
+    file_handle.close()
+    
+    metacyc_datafile=os.path.join(config.data_folder,
+        config.metacyc_reactions_to_pathways)
+
+    # Run minpath to identify the pathways
+    tmpfile=run_minpath(reactions_file, metacyc_datafile)
+    
+    # Overwrite the reactions file with the pathway data
+    pathways_file=reactions_file
+    
+    # Process the minpath results
+    file_handle_read=open(tmpfile, "r")
+    file_handle_write=open(pathways_file, "w")
+    
+    line=file_handle_read.readline()
+    
+    pathways_store={}
+    while line:
+        data=line.strip().split(config.minpath_pathway_delimiter)
+        if re.search("^path",line):
+            current_pathway=data[config.minpath_pathway_index]
+        else:
+            current_reaction=data[config.minpath_reaction_index]
+            # store the pathway and reaction
+            pathways_store[current_reaction]=pathways_store.get(
+                current_reaction,[]) + [current_pathway]      
+        line=file_handle_read.readline()
+
+    file_handle_read.close()
+    
+    # Write the pathway abundance for each reaction
+    for current_reaction in reactions_store:
+        # Find the pathways associated with reaction
+        for current_pathway in pathways_store.get(current_reaction,[""]):
+            new_line=config.output_file_column_delimiter.join([current_reaction,
+                current_pathway, str(reactions_store[current_reaction]),"\n"])
+            file_handle_write.write(new_line)
+    file_handle_write.close()
+   
+    # Return the name of the temp file with the pathways
+    return pathways_file
+    
+    
+def pathways(threads, alignments):
+    """
+    Identify the reactions and then pathways from the hits found
+    """
+    
+    # Install minpath
+    minpath_exe=install_minpath()
+    
+    # load in the reactions database
+    gene_to_reactions=os.path.join(config.data_folder,
+        config.metacyc_gene_to_reactions)
+    reactions_database=store.reactions_database(gene_to_reactions)
+    
+    pathways_files={}
+    # Set up a command to run through each of the hits by bug
+    # Also run on all of the hits at once
+    args=[]
+    for bug in alignments.bug_list()+["all"]:
+        
+        hits=alignments.hits_for_bug(bug)
+        
+        # Create a temp file for the reactions results
+        file_out, pathways_file=tempfile.mkstemp()
+        os.close(file_out)
+
+        pathways_files[bug]=pathways_file
+
+        args.append([reactions_database, hits, pathways_file, bug])
+        
+    utilities.command_multiprocessing(threads, args, function=pathways_by_bug)
+
+    return pathways_files
+
+def pathways_abundance_and_coverage(pathways_files):
+    """
+    Compute the abundance and coverage of the pathways
+    """
+
+    # Compute abundance
+    utilities.execute_command(
+        "cat",[],[pathways_files["all"]],[],
+        config.pathabundance_file, pathways_files["all"])
+
+    # Compute coverage 
+    utilities.execute_command(
+        "cat",[],[pathways_files["unclassified"]],[],
+        config.pathcoverage_file, pathways_files["unclassified"])
+    
+    # Remove the temp pathways files
+    for file in pathways_files:
+        utilities.remove_file(pathways_files[file])
+
+    return config.pathabundance_file, config.pathcoverage_file
