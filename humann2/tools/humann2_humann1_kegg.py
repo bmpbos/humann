db2d999339d59560c60bb1d1680e7a5dce69422a
Lauren McIver <Lauren McIver@localhost>
2015-04-28 11:04:17 -0600

    Add join taxonomic profiles script

---
+++ humann2/tools/humann2_humann1_kegg.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+from cStringIO import StringIO
+import sys,string
+import sys, os
+import argparse
+
+
+
+#********************************************************************************************
+#    Build kegg extract                                                                     *
+#                                                                                           *
+#    The objective of this program is to build a Kegg extract (Kegg ID Mapping)             *
+#    kegg_idmapping                                                                         *
+# Format of the Output:                                                                     *
+# reference \t gene \t length (nucleotides) \t bug                                          *
+#  hsa:399693 \t K0# \t 953*3 \t bug                                                        *
+#  takes 3 input files:                                                                     *
+#  humann1/data/genels                                                                      *
+#  A. Format (first 3 lines bug:gene \t length(aa)):                                        *
+#     hsa:399693	953                                                                     *
+#     hsa:401052	118                                                                     *
+#     hsa:285033	121                                                                     *
+#  B. humann1/data/koc (bug:gene to K0)                                                     *
+#     Format:                                                                               *
+#     K0# \t hsa:# \t hsa:#2                                                                *
+#  C. kegg file with bug names: http://www.genome.jp/kegg/catalog/org_list.html             *
+#                                                                                           *
+#  The program is invoked as follows:                                                       *
+#  python humann1_kegg.py --igenels ./data/genels   --ikoc  ./data/koc   --o output_extract *
+#                                                                                            *
+#
+#  The program tries to read the Kegg translation table OrgId -->OrgName that is located in  *
+#  ../data/misc/KeggOrgId2OrgNameTable.txt                                                   *
+#  And if that fails,  it tries to get that info from Kegg itself ( "http://www.genome.jp/kegg/catalog/org_list.html" )
+#                                                                                            *
+# Written by George Weingart  March 31, 2015   george.weingart@gmail.com                     *
+#********************************************************************************************
+
+
+
+
+
+#*************************************************************************************
+#* Parse Input parms                                                                 *
+#*************************************************************************************
+def read_params(x):
+	CommonArea = dict()	
+	parser = argparse.ArgumentParser(description='Humann1_kegg extract generator')
+	parser.add_argument('--igenels', action="store", dest='igenels',nargs='?')
+	parser.add_argument('--ikeggtrans', action="store", dest='ikeggtrans',nargs='?', default="http://www.genome.jp/kegg/catalog/org_list.html")
+	parser.add_argument('--ikoc', action="store", dest='ikoc',nargs='?')
+	parser.add_argument('--ikeggOrgId2OrgName', action="store", dest='ikeggOrgId2OrgName',nargs='?')
+	parser.add_argument('--o', action="store", dest='o',nargs='?')
+	CommonArea['parser'] = parser
+	return  CommonArea
+
+	
+
+#************************************************************************
+#*   Load the dictionary of OrgId --> OrgName                           *
+#* The routine takes its data from the following web-site               *
+#*  "http://www.genome.jp/kegg/catalog/org_list.html"                   *
+#************************************************************************
+def  ReadHtmlKegTable(CommonArea):
+		print "Trying to read the online kegg html table to translate OrgId to OrgName : http://www.genome.jp/kegg/catalog/org_list.html"
+		from bs4 import BeautifulSoup
+		import urllib2
+		
+		dOrgIdOrgName = dict()
+		f1  =  urllib2.urlopen(CommonArea['ikeggtrans'])
+		soup1 = BeautifulSoup(f1)
+
+		for child in soup1.recursiveChildGenerator():
+				name = getattr(child, "name", None)
+				try:
+					xchild = str(child)
+					xname = str(name)
+					if xname.startswith("a")  and xchild.startswith('<a href="/dbget-bin/www_'):
+						ychild = str(xchild)
+						OrgName = ychild.split(">")[1].split('<')[0]
+						OrgNameOrgId = OrgId + "\t" +  OrgName 
+						dOrgIdOrgName[OrgId] = OrgName
+						
+					if xname.startswith("a")     and xchild.startswith('<a href="/kegg-bin/show_organism?org='):
+						ychild = str(xchild)
+						OrgId = ychild.split(">")[1].split('<')[0]
+				except:
+					pass
+		CommonArea['dOrgIdOrgName'] = dOrgIdOrgName 
+		return CommonArea 
+
+
+
+#************************************************************************
+#*  Try to read the Sequential File                                     *
+#************************************************************************
+def  ReadSequentialTranslationFile(CommonArea):
+		if  CommonArea['ikeggOrgId2OrgName'] is not None:
+			CommonArea['ikeggOrgId2OrgName_file'] = open(CommonArea['ikeggOrgId2OrgName'])
+		else:
+			FileLocation = os.path.join(os.path.dirname(os.path.abspath(__file__)),os.pardir, "data/misc/KeggOrgId2OrgNameTable.txt")
+			CommonArea['ikeggOrgId2OrgName_file'] = open(FileLocation)  
+			
+			
+		for iKeggOrgLine in CommonArea['ikeggOrgId2OrgName_file']: 
+			OrgId = iKeggOrgLine.split("\t")[0]
+			OrgName = iKeggOrgLine.split("\t")[1].rstrip('\n')
+			CommonArea['dOrgIdOrgName'][OrgId]  = OrgName 
+		return CommonArea 
+
+#*************************************************************************************
+#*  Main Program                                                                     *
+#*************************************************************************************
+print "Program started"
+
+CommonArea = read_params( sys.argv )  # Parse command  
+parser = CommonArea['parser'] 
+results = parser.parse_args()
+
+CommonArea['igenels'] = results.igenels
+CommonArea['igenels_file'] = open(CommonArea['igenels'])	# Open the igenels file
+
+CommonArea['ikoc'] = results.ikoc
+CommonArea['ikoc_file'] = open(CommonArea['ikoc'])	# Open the ikoc file
+
+CommonArea['ikeggtrans'] = results.ikeggtrans
+
+CommonArea['oFile'] = results.o
+OutputFile = open(CommonArea['oFile'],'w')
+
+CommonArea['ikeggOrgId2OrgName'] = results.ikeggOrgId2OrgName
+
+
+
+
+CommonArea['dOrgIdOrgName'] = dict()
+try:
+	CommonArea  =  ReadSequentialTranslationFile(CommonArea)   #First,  try to read the sequential file in our directory
+except:
+	print "Reading the Table OrgId--> Name in ../data/pathways/KeggOrgId2OrgNameTable.txt Failed - will try the kegg website"
+	CommonArea  =  ReadHtmlKegTable(CommonArea)   #But if that fails - try directly from Kegg
+ 
+
+dBugGeneToKO = dict()
+#************************************************************************
+#*   Load the koc dictionary                                            *
+#************************************************************************
+for ikocLine in CommonArea['ikoc_file']: 
+    kocEntries  = ikocLine.split('\t')
+    KO = kocEntries[0]
+    kocEntries.pop(0)
+
+    for kocEntry in kocEntries:
+		Bug = kocEntry.split("#")[0]
+		Gene = kocEntry.split("#")[1].rstrip('\n')
+		BugGene = Bug + ":" + Gene
+		dBugGeneToKO[BugGene] = KO
+		
+		
+ 
+for iLine in CommonArea['igenels_file']: 
+	iLine = iLine.rstrip('\n')
+	iBug = iLine.split(":")[0]
+	iGene = iLine.split(":")[1].split("\t")[0]
+	iGeneLengthAA = int(iLine.split("\t")[1])
+	iGeneLengthNucleotide = 3 * iGeneLengthAA 
+	iBugName = "n/a"
+	try:
+		iBugName = CommonArea['dOrgIdOrgName'][iBug]
+	except:
+		iBugName = iBug
+	
+	BugGene = iBug + ":" + iGene
+	BugGene  = BugGene.upper()
+	try:
+		KOEntry = dBugGeneToKO[BugGene]
+	except:
+		KOEntry = "NoKONum"
+		
+	
+	try:
+		OrgName = CommonArea['dOrgIdOrgName'][iBug] 
+	except: 
+		OrgName = iBug	
+		
+	if  KOEntry != "NoKONum":
+		OutputLine = iBug + ":" + iGene + "\t" + KOEntry + "\t" + str(iGeneLengthNucleotide) + "\t" + OrgName + "\n"
+		OutputFile.write(OutputLine)
+	
+		
+OutputFile.close()
+print "Program ended Successfully"
+exit(0)
---
+++ humann2/tools/util.py
@@ -0,0 +1,162 @@
+from __future__ import print_function # PYTHON 2.7+ REQUIRED
+import os
+import sys
+import re
+import subprocess
+import csv
+import gzip
+
+# ---------------------------------------------------------------
+# utilities used by the split and join tables scripts
+# ---------------------------------------------------------------
+
+def find_exe_in_path(exe):
+    """
+    Check that an executable exists in $PATH
+    """
+    
+    paths = os.environ["PATH"].split(os.pathsep)
+    for path in paths:
+        fullexe = os.path.join(path,exe)
+        if os.path.exists(fullexe):
+            if os.access(fullexe,os.X_OK):
+                return True
+    return False
+
+def biom_to_tsv(biom_file, new_tsv_file, taxonomy=None):
+    """
+    Convert from a biom to tsv file
+    """
+
+    cmd=["biom","convert","-i",biom_file,"-o",new_tsv_file,"--to-tsv"]
+
+    # check if taxonomy is set (can be set to zero)
+    if taxonomy != None:
+        cmd+=["--header-key","taxonomy"]
+    
+    try:
+        if os.path.isfile(new_tsv_file):
+            os.unlink(new_tsv_file)
+        p_out = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+    except (EnvironmentError, subprocess.CalledProcessError):
+        command=" ".join(cmd)
+        sys.exit("Unable to convert biom file to tsv"+"\n"+command)
+        
+def tsv_to_biom(tsv_file, biom_file):
+    """
+    Convert from a biom to tsv file
+    """
+    
+    cmd=["biom","convert","-i",tsv_file,"-o",biom_file,"--table-type","Gene table","--to-hdf5"]
+
+    try:
+        if os.path.isfile(biom_file):
+            os.unlink(biom_file)
+        p_out = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+    except (EnvironmentError, subprocess.CalledProcessError):
+        command=" ".join(cmd)
+        sys.exit("Unable to convert tsv file to biom"+"\n"+command)
+        
+def process_gene_table_header(gene_table):
+    """
+    Process through the header portion of the gene table file
+    """
+    
+    # indicator of a comment line or the header
+    # the last line in the file with this indicator is the header
+    GENE_TABLE_COMMENT_LINE="^#"
+    
+    # try to open the file
+    try:
+        file_handle=open(gene_table,"r")
+        line=file_handle.readline()
+    except EnvironmentError:
+        sys.exit("Unable to read file: " + gene_table)
+            
+    # find the headers
+    header_flag=False
+    header=line
+    while re.match(GENE_TABLE_COMMENT_LINE, line):
+        header_flag=True
+        header = line
+        line = file_handle.readline()
+            
+    if not header_flag:
+        sys.exit("File does not have a required header: " + gene_table +
+            " . Please add a header which includes the indicator: " +
+            GENE_TABLE_COMMENT_LINE)
+        
+    return file_handle, header, line
+
+# ---------------------------------------------------------------
+# utilities used by the rename, renorm scripts
+# ---------------------------------------------------------------
+
+# constants
+c_strat_delim = "|"
+c_name_delim = ": "
+c_multiname_delim = ";"
+c_str_unknown = "NO_NAME"
+
+class Table ( ):
+
+    """ 
+    Very basic table class; would be more efficient using numpy 2D array
+    """
+
+    def __init__ ( self, path ):
+        self.anchor = None
+        self.colheads = []
+        self.rowheads = []
+        self.data = []
+        self.is_stratified = False
+        if path is None:
+            fh = sys.stdin
+            path = "STDIN"
+            print( "Loading table from STDIN", file=sys.stderr )
+        else:
+            fh = try_zip_open( path )
+        for row in csv.reader( fh, dialect='excel-tab' ):
+            if self.anchor is None:
+                self.anchor = row[0]
+                self.colheads = row[1:]
+            else:
+                self.rowheads.append( row[0] )
+                self.data.append( row[1:] )
+        for rowhead in self.rowheads:
+            if c_strat_delim in rowhead:
+                print( "Treating", path, "as stratified output, e.g.", 
+                       rowhead.split( c_strat_delim ), file=sys.stderr )
+                self.is_stratified = True
+                break
+
+    def write ( self, path=None ):
+        fh = try_zip_open( path, "w" ) if path is not None else sys.stdout
+        writer = csv.writer( fh, dialect='excel-tab' )
+        writer.writerow( [self.anchor] + self.colheads )
+        for i in range( len( self.rowheads ) ):
+            writer.writerow( [self.rowheads[i]] + self.data[i] )
+
+def try_zip_open( path, *args ):
+    """ 
+    open an uncompressed or gzipped file; fail gracefully 
+    """
+    fh = None
+    try:
+        fh = open( path, *args ) if not re.search( r".gz$", path ) else gzip.GzipFile( path, *args )
+    except:
+        print( "Problem opening", path, file=sys.stderr )
+    return fh
+
+def load_polymap ( path ):
+    """ 
+    load a tsv file mapping one name to another (e.g. uniref50 id to english name)
+    """
+    polymap = {}
+    with try_zip_open( path ) as fh:
+        for row in csv.reader( fh, dialect="excel-tab" ):
+            old_name = row[0]
+            for new_name in row[1:]:
+                polymap.setdefault( old_name, {} )[new_name] = 1
+    print( "Loaded polymap from", path, file=sys.stderr )
+    return polymap
